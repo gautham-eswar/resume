5445d063976eb5419a8f5b6e3f5b18fcab70011a
gauthameswar <gautham1202@gmail.com>
2025-04-14 00:18:58 -0700

    Fix ASGI configuration by using Flask's wsgi_app

--- asgi.py
+++ asgi.py
@@ -2,4 +2,4 @@ from app import app
 from asgiref.wsgi import WsgiToAsgi
 
 # Create an ASGI application by wrapping the Flask WSGI app
-asgi_app = WsgiToAsgi(app) 
+asgi_app = WsgiToAsgi(app.wsgi_app) 
